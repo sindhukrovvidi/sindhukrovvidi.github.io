8c605b96386e25ca1105967a7f692217571650dc
Sindhu Krovvidi <sindhukrovvidi@gmail.com>
2024-07-17 00:44:08 -0400

    Added about

--- src/data/Bio.js
+++ src/data/Bio.js
@@ -1,3 +1,18 @@
 export const bioData = [
-
+	{
+		id: 1,
+		para: "I am a dedicated and accomplished Software Development Engineer with a Master's degree in Computer Science from Northeastern University, Boston.",
+	},
+	{
+		id: 2,
+		para: "Throughout my academic and professional experience, I have developed a strong skill set that encompasses a wide range of programming languages and technologies. From Java to C# to JavaScript frameworks like React.js and Angular, I can create both frontend interfaces and solid backend systems. My expertise includes cloud technologies such as AWS, Docker, and CI/CD pipelines, allowing me to develop scalable, robust apps that meet modern industry standards."
+	},
+	{
+		id: 3,
+		para: "I was recognized for my contributions and received the `Quality Award` for demonstarting problem-solving skills and driving business results. I mentored 5 junior team members, led a team and organized scrums and events."
+	},
+	{
+		id: 4,
+		para: "As a dedicated software engineer, I bring a proactive problem-solving approach and a passion for innovation. Let's connect and discuss how I can contribute to your team's success."
+	}
 ];
